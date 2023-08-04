d7b28d4fc6380b351639e0ad35b33409dc3e5487
Sarthak Bansal <sbansal793@gmail.com>
2023-08-04 15:38:53 +0530

    completed section 2

--- Desktop/templates/home/Element2/element2.js
+++ Desktop/templates/home/Element2/element2.js
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import styles from "./element2.module.css";
-import img3 from "../../../public/dstatic/home/element2/f1ls.png";
-import img4 from "../../../public/dstatic/home/element2/trackls.png";
-import img9 from "../../../public/dstatic/home/element2/clockls.png";
+
 const Element2 = () => {
   return (
     <div className={styles.element2}>
@@ -12,7 +10,11 @@ const Element2 = () => {
             <Image src="/dstatic/home/element2/car1short.png" fill={true} />
           </div>
           <div className={styles.carImg2}>
-            <Image className={styles.carImgT} src="/dstatic/home/element2/car1c.png" fill={true} />
+            <Image
+              className={styles.carImgT}
+              src="/dstatic/home/element2/car1c.png"
+              fill={true}
+            />
           </div>
           <div className={styles.cardInfo}>
             <h4 className={styles.cardTitle1}>
@@ -24,35 +26,46 @@ const Element2 = () => {
               auto-racing from the sound of engines revving to the thrill of the
               race.
             </p>
-            <Image src={img3} alt="" className={styles.f1ls} />
+            <div className={styles.f1ls}>
+              <Image src="/dstatic/home/element2/f1ls.png" alt="" fill={true} />
+            </div>
           </div>
         </div>
         <div className={styles.element2row}>
           <div className={styles.cardInfo2}>
-            <div className={styles.cardTitle2}>
-              An Open Track for Car Lovers
-            </div>
-            <div className={styles.cardDescription}>
+            <h4 className={styles.cardTitle2}>An Open Track for Car Lovers</h4>
+            <p className={styles.cardDescription}>
               Whether you&apos;re a seasoned pro or just getting started, our
               community is the perfect place to connect with other fans, ask
               questions, get answers, share your experiences, and earn
               reputation points by interacting with others.
+            </p>
+            <div className={styles.trackls}>
+              <Image
+                src="/dstatic/home/element2/trackls.png"
+                className={styles.setPositionInitial}
+                alt=""
+                fill={true}
+              />
             </div>
-            <Image src={img4} alt="" className={styles.trackls} />
           </div>
           <div className={styles.carImg}>
-          <Image src="/dstatic/home/element2/car2short.png" fill={true} />
+            <Image src="/dstatic/home/element2/car2short.png" fill={true} />
           </div>
           <div className={styles.carImg2}>
-          <Image src="/dstatic/home/element2/car2c.png" fill={true}/>
-          </div>     
+            <Image src="/dstatic/home/element2/car2c.png" fill={true} />
+          </div>
         </div>
         <div className={styles.element2row}>
-        <div className={styles.carImg}>
+          <div className={styles.carImg}>
             <Image src="/dstatic/home/element2/car3short.png" fill={true} />
           </div>
           <div className={styles.carImg2}>
-            <Image className={styles.carImgT} src="/dstatic/home/element2/car3c.png" fill={true} />
+            <Image
+              className={styles.carImgT}
+              src="/dstatic/home/element2/car3c.png"
+              fill={true}
+            />
           </div>
           <div className={styles.cardInfo3}>
             <h4 className={styles.cardTitle3}>STAY UPDATED ON THE GO</h4>
@@ -65,7 +78,14 @@ const Element2 = () => {
               motorheads to access anytime, share their thoughts, or interact
               with other users on the latest trends.
             </div>
-            <Image src={img9} alt="" className={styles.clockls} />
+            <div className={styles.clockls}>
+              <Image
+                src="/dstatic/home/element2/clockls.png"
+                alt="stopwatch"
+                className={styles.setPositionInitial}
+                fill={true}
+              />
+            </div>
           </div>
         </div>
         <h4 className={styles.element2row3}>
--- Desktop/templates/home/Element3/element3.js
+++ Desktop/templates/home/Element3/element3.js
@@ -1,165 +1,91 @@
-import Image from 'next/image';
-import styles from './element3.module.css';
-import img1 from '../../../public/dstatic/home/element3/ask.png';
-import img4 from '../../../public/dstatic/home/element3/formulacar.webp';
-import img2 from '../../../public/dstatic/home/element3/help.png';
-import img3 from '../../../public/dstatic/home/element3/share.png';
-import img5 from '../../../public/dstatic/home/element3/formulacarshort.png';
-import img6 from '../../../public/dstatic/home/element3/steering.png';
+import Image from "next/image";
+import styles from "./element3.module.css";
+import img1 from "../../../public/dstatic/home/element3/ask.png";
+import img4 from "../../../public/dstatic/home/element3/formulacar.webp";
+import img2 from "../../../public/dstatic/home/element3/help.png";
+import img3 from "../../../public/dstatic/home/element3/share.png";
+import img5 from "../../../public/dstatic/home/element3/formulacarshort.png";
+import img6 from "../../../public/dstatic/home/element3/steering.png";
 
 const Element3 = () => {
-	return (
-		<div className={styles.element3}>
-			<div className={styles.element3row}>
-				<div
-					className={
-						styles.element3container
-					}
-				>
-					<div
-						className={styles.leftUpperText}
-					>
-						what <br></br>can you <br></br>do
-						with<br></br> racingline?
-					</div>
-					<div className={styles.upperflex}>
-						<div>
-							<span>
-								<Image
-									src={img1}
-									className={styles.icons}
-								/>
-							</span>
-							<span>
-								<div
-									className={styles.upperTitle}
-								>
-									ASK QUESTIONS
-								</div>
-								<div
-									className={
-										styles.upperDescription
-									}
-								>
-									Have a burning question about
-									auto racing? Our community is
-									the perfect place to get
-									answers from other experts and
-									enthusiasts.
-								</div>
-							</span>
-						</div>
-						<div>
-							<span>
-								<Image
-									src={img2}
-									className={styles.icons}
-								/>
-							</span>
-							<span>
-								<div
-									className={styles.upperTitle}
-								>
-									HELP OTHERS
-								</div>
-								<div
-									className={
-										styles.upperDescription
-									}
-								>
-									If you&apos;re an expert in the
-									field or have a knack for the
-									field, you can help answer
-									questions and share your
-									knowledge with others.
-								</div>
-							</span>
-						</div>
-						<div>
-							<span>
-								<Image
-									src={img3}
-									className={styles.icons}
-								/>
-							</span>
-							<span>
-								<div
-									className={styles.upperTitle}
-								>
-									SHARE YOUR VIEWS
-								</div>
-								<div
-									className={
-										styles.upperDescription
-									}
-								>
-									Comment your thoughts,
-									experiences, and opinions on
-									all things auto racing, from
-									the latest news to
-									people&apos;s opinions.
-								</div>
-							</span>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className={styles.element3row2}>
-				<div className={styles.lowerText}>
-					With Racingline, interacting with
-					people just like you has become
-					easier.
-					<span className={styles.hidden}>
-						s
-					</span>
-					<span
-						className={styles.lowerTextRed}
-					>
-						Finding your <br />
-						own community and group of car
-						lovers is just a few clicks away.
-					</span>
-				</div>
-				<div className={styles.lowerText2}>
-					<span
-						className={styles.lowerTextRed}
-					>
-						Here on the platform,
-					</span>
-					<span className={styles.hidden}>
-						s
-					</span>
-					connect with other fans and
-					enthusiasts from
-					<br /> around the world, make new
-					friends, and share your love of{' '}
-					<br /> sport.
-				</div>
-				<Image
-					src={img4}
-					className={styles.formulacar}
-				/>
-				<Image
-					src={img5}
-					className={styles.formulacarshort}
-				/>
-			</div>
-			<div className={styles.element3row3}>
-				<span className={styles.line1} />
-				<Image
-					src={img6}
-					className={styles.steering}
-				/>
-				<div className={styles.textcenter}>
-					Go on and take the wheel today!
-				</div>
-				<button className={styles.button}>
-					GO TO FEED
-				</button>
-				<span className={styles.line2} />
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.element3}>
+      <div className={styles.element3row}>
+        <div className={styles.element3container}>
+          <div className={styles.leftUpperText}>
+            what <br></br>can you <br></br>do with<br></br> racingline?
+          </div>
+          <div className={styles.upperflex}>
+            <div>
+              <span>
+                <Image src={img1} className={styles.icons} />
+              </span>
+              <span>
+                <div className={styles.upperTitle}>ASK QUESTIONS</div>
+                <div className={styles.upperDescription}>
+                  Have a burning question about auto racing? Our community is
+                  the perfect place to get answers from other experts and
+                  enthusiasts.
+                </div>
+              </span>
+            </div>
+            <div>
+              <span>
+                <Image src={img2} className={styles.icons} />
+              </span>
+              <span>
+                <div className={styles.upperTitle}>HELP OTHERS</div>
+                <div className={styles.upperDescription}>
+                  If you&apos;re an expert in the field or have a knack for the
+                  field, you can help answer questions and share your knowledge
+                  with others.
+                </div>
+              </span>
+            </div>
+            <div>
+              <span>
+                <Image src={img3} className={styles.icons} />
+              </span>
+              <span>
+                <div className={styles.upperTitle}>SHARE YOUR VIEWS</div>
+                <div className={styles.upperDescription}>
+                  Comment your thoughts, experiences, and opinions on all things
+                  auto racing, from the latest news to people&apos;s opinions.
+                </div>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.element3row2}>
+        <div className={styles.lowerText}>
+          With Racingline, interacting with people just like you has become
+          easier.
+          <span className={styles.hidden}>s</span>
+          <span className={styles.lowerTextRed}>
+            Finding your <br />
+            own community and group of car lovers is just a few clicks away.
+          </span>
+        </div>
+        <div className={styles.lowerText2}>
+          <span className={styles.lowerTextRed}>Here on the platform,</span>
+          <span className={styles.hidden}>s</span>
+          connect with other fans and enthusiasts from
+          <br /> around the world, make new friends, and share your love of{" "}
+          <br /> sport.
+        </div>
+        <Image src={img4} className={styles.formulacar} />
+        <Image src={img5} className={styles.formulacarshort} />
+      </div>
+      <div className={styles.element3row3}>
+        <span className={styles.line1} />
+        <Image src={img6} className={styles.steering} />
+        <div className={styles.textcenter}>Go on and take the wheel today!</div>
+        <button className={styles.button}>GO TO FEED</button>
+        <span className={styles.line2} />
+      </div>
+    </div>
+  );
 };
 
 export default Element3;
